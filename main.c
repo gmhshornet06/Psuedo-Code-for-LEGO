e3de1c827a419882bd87087c8cec1e3e7d405a73
gmhshornet06 <47538676+gmhshornet06@users.noreply.github.com>
2019-02-13 14:22:16 -0500

    Psuedo Code for LEGO
    psuedo code

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <kipr/botball.h>
+
+int main
+{
+	//pseudocode
+	//define functions
+	//119 second timer
+	//LightStart
+	//center robot on ambulance
+	//lower claw
+	//close claw
+	//life claw
+	//move forward 13 inches
+	//continue moving until blue surface is detected
+	//continue moving until white surface is detected
+	//turn 90 degrees left
+	//continue moving until white is detected
+	//close plow
+	//move forward 10 inches
+	//if medical center is burning;
+		//open plow
+		//move back 5 inches
+		//close plow
+		//move forward 5 inches
+	//else;
+		//lower claw
+		//open claw
+		//lift claw
+	//turn 90 degrees left
+	//move 14 inches
+	//turn 90 degrees right
+	//if medical center is burning;
+		//open plow
+		//move back 5 inches
+		//close plow
+		//move forward 5 inches
+	//else;
+		//lower claw
+		//open claw
+		//lift claw
+	//turn 90 degrees right
+	//move 15 inches forward
+	//turn 90 degrees right
+	//move 15 inches forward
+	//turn 90 degrees left
+	//move 8 inches forward
+	//close plow
+	//move 8 inches back
+	//turn 90 degrees right
+	//move 15 inches forward
+	//turn 90 degrees left
+	//move 8 inches forward
+	//close plow
+    
